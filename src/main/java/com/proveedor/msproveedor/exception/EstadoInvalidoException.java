df04157441052fb69b22f7cc37b6d355177ddae7
Flosotoo <flor.soto@duocuc.cl>
2026-06-21 15:15:41 -0400

    exceptions listos

---
+++ src/main/java/com/proveedor/msproveedor/exception/EstadoInvalidoException.java
@@ -0,0 +1,7 @@
+package com.proveedor.msproveedor.exception;
+
+public class EstadoInvalidoException extends RuntimeException {
+    public EstadoInvalidoException(String mensaje) {
+        super(mensaje);
+    }
+}
---
+++ src/main/java/com/proveedor/msproveedor/exception/RecursoNoEncontradoException.java
@@ -0,0 +1,7 @@
+package com.proveedor.msproveedor.exception;
+
+public class RecursoNoEncontradoException extends RuntimeException{
+    public RecursoNoEncontradoException(String mensaje) {
+        super(mensaje);
+    }
+}
